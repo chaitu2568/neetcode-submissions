b7d6abab404d0daaf68575362132a6c21898d537
Krishna Chaitanya Bogavalli <47073242+chaitu2568@users.noreply.github.com>
2026-05-19 19:39:29 -0500

    Add: next-permutation - submission-0

---
+++ Data Structures & Algorithms/next-permutation/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        i = len(nums)-2
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
+        
+        if i >= 0:
+            j = len(nums)-1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i],nums[j] = nums[j],nums[i]
+
+        first, second = i+1, len(nums)-1
+
+        while first <= second:
+            nums[first],nums[second] = nums[second],nums[first]
+            first += 1
+            second -= 1
+
+
+
+
+        
+
+        
