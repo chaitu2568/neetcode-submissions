5f2dcf97f3704091cb1ff21b06fa116b161c30c7
Krishna Chaitanya Bogavalli <47073242+chaitu2568@users.noreply.github.com>
2026-06-18 16:35:48 -0500

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        l,r = 0,x
+
+        while l <= r:
+
+            mid = (l+r)//2
+
+            temp = mid*mid
+
+            if temp == x:
+                return mid
+            
+            elif temp < x:
+                l = mid + 1
+            
+            else:
+                r = mid - 1
+        
+        return r
+        
